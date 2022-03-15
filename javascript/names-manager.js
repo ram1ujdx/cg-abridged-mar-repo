2b40970679f4f0a44f785c74df46423db7924366
Ramanuj DS <ramanujds9@gmail.com>
2022-03-15 16:50:54 +0530

    javascript es6

--- javascript/names-manager.js
+++ javascript/names-manager.js
@@ -1,16 +1,33 @@
-var names=[];
-function addNames(){
+var persons=[];
+let addPerson = function(){
     let name = document.getElementById("nametxt").value;
-    names.push(name);
+    let email = document.getElementById("emailtxt").value;
+    let age = document.getElementById("agetxt").value;
+    let person = new Person(name,email,age);
+    persons.push(person);
     refreshList();
 
 }
 
+let fun = personName => personName.toString().toUpperCase();
+
 
 function refreshList(){
     let txt = "";
-    for(let i=0; i<names.length; i++){
-        txt+="<li>"+names[i]+"</li>";
+    for(let i=0; i<persons.length; i++){
+        txt+="<li>"+persons[i].toString()+"</li>";
+        console.log(persons[i])
     }
     document.getElementById("namelist").innerHTML=txt;
+}
+
+class Person{
+    constructor(name,email,age){
+        this.name=name;
+        this.age=age;
+        this.email=email;
+    }
+    toString(){
+        return this.name+" -- "+this.email+" -- "+this.age;
+    }
 }
